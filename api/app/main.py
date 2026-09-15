2247284d782da4a9d212a063272a7838bd29e938
nullpointer-01 <223323887+NullPointer-01@users.noreply.github.com>
2026-09-15 21:48:58 +0530

    Add required python dependencies

---
+++ api/app/main.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Hello, Api!")
+
+if __name__ == "__main__":
+    main()
---
+++ crawler/app/main.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Hello, Crawler!")
+
+if __name__ == "__main__":
+    main()
